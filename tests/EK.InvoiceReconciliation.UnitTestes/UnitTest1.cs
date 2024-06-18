c8d167c82315452c0744675f5e100c1710d5e5d2
EvertonKluczkovski <kluczkovski@hotmail.com>
2024-06-18 21:39:36 +0100

    add unit test

--- tests/EK.InvoiceReconciliation.UnitTestes/UnitTest1.cs
+++ tests/EK.InvoiceReconciliation.UnitTestes/UnitTest1.cs
@@ -5,6 +5,14 @@ public class UnitTest1
     [Fact]
     public void Test1()
     {
+        //Arrange
+        var a = 10;
+        var b = 20;
 
+        //Act
+        var c =  a + b;
+
+        //Assert
+        Assert.Equal(30, c);
     }
 }
